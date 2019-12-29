fef16fb7bfd80beb5f0dd4ef011e4a27ea3aa7aa
Michael Chen <michael@MichaeldeMBP.lan>
2019-12-29 22:20:58 +0800

    fix conflict

--- comCode/api_object.py
+++ comCode/api_object.py
@@ -1,9 +1,8 @@
-
 class tvApi:
     class tvApiParam:
         def __init__(self, name):
             self.name = name
     def __init__(self, id
                  ):
-        tap =
+        pass
 p = tvApiParam("tom")
