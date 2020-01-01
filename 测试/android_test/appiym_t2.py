fc89bb9cb2846eee9f92dec53b31f2d097c475ed
Michael Chen <michael@MichaeldeMBP.lan>
2020-01-01 22:25:15 +0800

    app_auto

--- 测试/android_test/appiym_t2.py
+++ 测试/android_test/appiym_t2.py
@@ -1,6 +1,8 @@
 from appium import webdriver
 import time
 
+from selenium.webdriver.support.wait import WebDriverWait
+
 desired_caps = {}
 desired_caps['platformName'] = 'Android'  # 系统名称
 desired_caps['platformVersion'] = '6.0'  # 系统的版本号
@@ -25,15 +27,38 @@ driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 #     driver.install_app("/Users/michael/Desktop/app")#安装app
 # driver.close_app()#只关闭操作的app，不关闭驱动对象
 
-#将应用置于后台5秒钟再返回前台
+# 将应用置于后台5秒钟再返回前台
 # driver.background_app(5)
 # time.sleep(5)
 # driver.close_app()#只关闭操作的app，不关闭驱动对象
 
-#元素定位
+###元素定位###
+# 定位一个元素
 # driver.find_element_by_id()
 # driver.find_element_by_class_name()
 # driver.find_element_by_xpath()
+# train
+# driver.find_element_by_id("com.android.settings:id/search").click()
+# driver.find_element_by_class_name("android.widget.EditText").send_keys("ssss")
+# driver.find_element_by_xpath("//*[@context-desc='收起]").click()
+# 定位一组元素
+# textviews = driver.find_element_by_class_name("android.widget.TextView")
+# for textview in textviews:
+#     print(textview.text)
+# eles = driver.find_elements_by_xpath("//*[contains(@text,'设')]")
+# for i in eles:
+#     print(i.text)
+
+###元素等待###
+# 1、隐式等待（全局有效）
+# 通常用于针对所有定位元素的超时时间设置为同一个值的时候
+# driver.implicitly_wait(200)#等待200秒寻找按钮
+# driver.find_elements_by_xpath("//*[@content-desc='收起']").click()
+# 2、显式等待（单个寻找有效）
+# 通常用于针对所有定位元素的超时时间设置为不同值的时候
+# wait = WebDriverWait(driver,5,1)#设置5秒每1秒去寻找一次
+# back_button = wait.until(lambda x:x.find_elements_by_xpath("//*[@content-desc='收起']"))
+# back_button.click()
 
 
 driver.quit()  # 关闭驱动对象，同时关闭所有关联的app
