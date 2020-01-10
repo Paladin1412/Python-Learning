430e2c377fea1f359dd556ceed0e2c60556a98c5
“michael <354307169@qq.com>
2020-01-10 11:22:38 +0800

    1.9

--- python异步编程/协程/async_await.py
+++ python异步编程/协程/async_await.py
@@ -1,19 +1,23 @@
-import types
-
-
-@types.coroutine
-def downloader(url):
-    yield 'm'
-
-# async def downloader(url):
-#     return "m"
-
-
-async def download_url(url):
-    html = await downloader(url)
-    return html
-
-
-if __name__ == "__main__":
-    coro = download_url("www")
-    coro.send(None)
+# import types
+#
+#
+# @types.coroutine
+# def downloader(url):
+#     yield 'm'
+#
+# # async def downloader(url):
+# #     return "m"
+#
+#
+# async def download_url(url):
+#     html = await downloader(url)
+#     return html
+#
+#
+# if __name__ == "__main__":
+#     coro = download_url("www")
+#     coro.send(None)
+a=[1,2]
+b=[1,2]
+if a == b:
+    print(1)
