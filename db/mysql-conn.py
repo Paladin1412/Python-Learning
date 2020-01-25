d59ab6723b671daf3d2b29f2ccbb50e458d6c0b3
michaelanony <354307169@qq.com>
2020-01-25 15:24:07 +0800

    1.25

---
+++ db/mysql-conn.py
@@ -0,0 +1,8 @@
+import pymysql
+
+db = pymysql.connect(host='192.168.11.31',port=30001,user='michael',password='cccbbb',database='home')
+with db.cursor() as cursor:
+    cursor.execute("select * from lywebback_papers")
+    ret = cursor.fetchall()
+print(ret)
+db.close()
