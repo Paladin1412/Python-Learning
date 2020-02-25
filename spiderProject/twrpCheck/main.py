47429bd0217c46d098db6f91487cc2ce6a20fa88
michaelanony <354307169@qq.com>
2020-02-25 16:35:22 +0800

    add twrp spider

---
+++ spiderProject/twrpCheck/main.py
@@ -0,0 +1,32 @@
+import requests
+import urllib3
+from lxml import etree
+import redis
+import time
+import random
+import sys
+
+
+LOOP_FLAG = True
+
+while True:
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    url = "https://twrp.me/Devices/Xiaomi/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 NewsArticle/7.5.7.32 JsSdk/2.0 NetType/WIFI (News 7.5.7 13.300000)",
+        "Accept": "application/json, text/javascript"
+    }
+    sess = requests.session()
+    sess.headers = headers
+    sess.verify = False
+    ret_page = sess.get(url=url).text
+    e_page = etree.HTML(ret_page)
+    ret = e_page.xpath('//ul[@class="post-list"]/p/strong/a/text()')
+    for i in ret:
+        if "10" in i:
+            LOOP_FLAG = False
+            rc = redis.Redis(host="192.168.11.31", port="30002")
+            rc.set("twrpCheck", "True")
+            print("TWRP CHECK EXIST!!!")
+            sys.exit(0)
+    time.sleep(60 * random.uniform(1, 2))
