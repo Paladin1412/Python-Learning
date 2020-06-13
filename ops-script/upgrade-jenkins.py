def04a3da0bc1fc7e07793a35c637569bdbfac82
michaelanony <354307169@qq.com>
2020-06-13 11:45:12 +0800

    change subprocess

--- ops-script/upgrade-jenkins.py
+++ ops-script/upgrade-jenkins.py
@@ -7,11 +7,6 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s', )
 
 
-def call_sys(command):
-    ret = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    return ret.stdout.read().decode('utf-8')
-
-
 def start(argv):
     version = 'latest'
     logging.info("Init...")
@@ -24,21 +19,16 @@ def start(argv):
             version = args
     logging.info("Get version: {}".format(version))
     logging.info("Try to pull from registry...")
-    ret = call_sys("docker pull jenkins/jenkins:{}".format(version))
-    logging.info(ret)
+    subprocess.call("docker pull jenkins/jenkins:{}".format(version))
     logging.info("Try to push to local registry...")
-    ret = call_sys("docker tag jenkins/jenkins:{} 192.168.11.3:10000/home/jenkins:{}".format(version, version))
-    logging.info(ret)
-    ret = call_sys("docker push 192.168.11.3:10000/home/jenkins:{}".format(version))
-    logging.info(ret)
+    subprocess.call("docker tag jenkins/jenkins:{} 192.168.11.3:10000/home/jenkins:{}".format(version, version))
+    subprocess.call("docker push 192.168.11.3:10000/home/jenkins:{}".format(version))
     logging.info("Changing jenkins pod image...")
-    ret = call_sys(
+    subprocess.call(
         "kubectl set image -n ci --record deployment jenkins-ci jenkins-ci=192.168.11.3:10000/home/jenkins:{}".format(
             version))
-    logging.info(ret)
-    ret = call_sys("kubectl rollout status deploy jenkins-ci -n ci")
-    logging.info(ret)
-
+    subprocess.call("kubectl rollout status deploy jenkins-ci -n ci")
+    logging.info("SUCCESS!")
 
 if __name__ == '__main__':
     start(sys.argv[1:])
